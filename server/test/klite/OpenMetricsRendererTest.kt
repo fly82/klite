eee18fdeb672b5159773a95c46cf375447f3021d
Anton Keks <anton@azib.net>
2025-12-11 14:28:08 +0200

    update renderer test

--- server/test/klite/OpenMetricsRendererTest.kt
+++ server/test/klite/OpenMetricsRendererTest.kt
@@ -8,14 +8,11 @@ import java.io.ByteArrayOutputStream
 class OpenMetricsRendererTest {
   @Test fun render() {
     val out = ByteArrayOutputStream()
-    OpenMetricsRenderer().render(out, mapOf("myNumber" to 42, "myMap" to mapOf("aValue" to 3.14), "myInfo" to "text"))
-    expect(out.toString().replace("\r\n", "\n")).toEqual("""
-      # TYPE my_number counter
-      my_number 42
-      # TYPE my_map_a_value gauge
-      my_map_a_value 3.14
-      # TYPE my_info info
-      my_info{value="text"} 1
+    OpenMetricsRenderer(keyPrefix = "test").render(out, mapOf("myNumber" to 42, "myMap" to mapOf("aValue" to 3.14), "myInfo" to "text"))
+    expect(out.toString()).toEqual("""
+      test_my_number 42
+      test_my_map_a_value 3.14
+      test_my_info{value="text"} 1
       # EOF
     """.trimIndent() + "\n")
   }
